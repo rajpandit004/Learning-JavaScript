9df9cdcc5702487ad953e49deec1a17c7da2c794
Raj Pandit <panditdev639@gmail.com>
2026-03-21 14:07:27 +0530

    datatype conversion done

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,16 +1,57 @@
+// ***** Conversion into Number *****
 let score = "33";
+let value_in_number = Number(score);
 
-// There are 2 syntax for 'typeof'
-console.log(typeof score); // It returns 'string'
-console.log(typeof(score));
+/*
+Syntax for 'typeof':
+- console.log(typeof score);
+- console.log(typeof(score));
+*/
 
-let value_in_number = Number(score); // It convert string to number
-console.log(typeof value_in_number); // It returns 'number'
-
-console.log(value_in_number)
-
-score = "33abc";
-value_in_number = Number(score);
-
-console.log(typeof value_in_number);
 console.log(value_in_number);
+console.log(typeof value_in_number);
+
+/*
+1. "33" => 33
+2. "33abc" => NaN
+3. "" => 0
+4. null => 0
+5. undefined => NaN
+6. true => 1, false => 0
+*/
+
+/*
+NOTE :-
+- NaN => Not a Number, but its type is still 'number'
+*/
+
+// ***** Conversion into Boolean *****
+let is_logged_in = 1;
+let boolean_is_logged_in = Boolean(is_logged_in);
+
+console.log(boolean_is_logged_in);
+console.log(typeof boolean_is_logged_in);
+
+/*
+1. 1 => true, 0 => false
+2. "" => false
+3. "text" => true
+4. null => false
+5. undefined => false
+*/
+
+// ***** Conversion into String *****
+let some_number = 33;
+let string_some_number = String(some_number);
+
+console.log(string_some_number);
+console.log(typeof string_some_number);
+
+/*
+1. 33 => "33"
+2. true => "true"
+3. false => "false"
+4. null => "null"
+5. undefined => "undefined"
+6. NaN => "NaN"
+*/
